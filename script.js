93dc7868d4434d70cf9003324504308b4fe734e8
Luis Brescia <luisfelipebrescia@gmail.com>
2023-08-27 18:45:37 -0300

    Introdução de novas leaderline

--- script.js
+++ script.js
@@ -1,46 +1,79 @@
 /*
 ? Documentação:
-
+function fluxoConecta(element) - Configura conexão entre dois elementos
+customDrag(elemento) - Configura as regras para dos elementos movíveis
+criaConteudo(id) - HTML de elemento do diagrama
+apagaElemento(element) - Apaga um elemento caso o ambiente esteja em modo exclusão
 */
 
-$(document).ready(function () {
+// * Estilização da linha
+var options = {
+    size: 5,
+    layer: 0,
+    endPlug: 'behind',
+    startSocket: 'right',
+    endSocket: 'left',
+    startPlugColor: '#0cf',
+    endPlugColor: '#04f',
+    gradient: true,
+    // color: '#04f',
+};
 
-    $('#alteraModo').click(function () {
-        console.log("ModoAlterado");
-        $('#alteraModo i').toggleClass('arrows-move bi-trash2');
-        $('#alteraModo span').text(function (i, text) {
-            return text === "Modo Exclusão" ? "Modo Edição" : "Modo Exclusão";
+// * Função para configurar regras para conexão
+function fluxoConecta(element) {
+    element.find('.linkInsert').mousedown(function (e) {
+
+        e.preventDefault();
+
+        let id = $(this).attr('id');
+        let idNumber = parseInt(id.split('-')[1]);
+        console.log("id: " + id);
+        const mouseX = e.clientX + pageXOffset;
+        const mouseY = e.clientY + pageYOffset;
+
+        let elmpoint = $(`<div id="elmpoint" style="top: ${mouseY}px; left: ${mouseX}px;"></div>`);
+        $('section').append(elmpoint);
+
+        let elmPoint = document.getElementById('elmpoint');
+        let mouseEl = document.getElementById('linkInsert-' + idNumber);
+
+        let linhaMouse = new LeaderLine(mouseEl, elmPoint, {
+            size: 5,
+            endPlug: 'disc',
+            startSocket: 'center',
+            endSocket: 'center',
+            path: 'grid',
+            startPlugColor: '#87ffcf',
+            endPlugColor: '#f5f9f8',
+            gradient: true
+        });
+
+        $(document).mousemove(function (e) {
+            elmPoint.style.left = `${e.clientX + pageXOffset}px`;
+            elmPoint.style.top = `${e.clientY + pageYOffset}px`;
+            linhaMouse.position();
+        });
+
+        $(document).mouseup(function (event) {
+
+            console.log("Coordenadas X:", event.pageX);
+            console.log("Coordenadas Y:", event.pageY);
+
+            if ($(event.target).is(".elemento-movivel-imagem, .elemento-movivel-nome")) {
+                console.log("mouse-solto em elemento");
+
+                // Será criado uma nova LeaderLine
+
+            }
+
+            console.log("mouse-solto");
+            linhaMouse.remove();
+            elmpoint.remove();
+            $(document).off('mousemove');
+            $(document).off('mouseup');
         });
     });
-    $('#exportaDados').click(function () {
-        console.log("exportarDados");
-    });
-
-    $('#adicionaElemento').click(function () {
-
-        // // * Realizar testes para checar se foi corrigido
-        // var lista = new Lista(indiceTrue, 'Lista ' + (indiceTrue + 1), " ", null);
-        // var newCard = criaConteudo(lista, indiceTrue);
-
-        // customDrag(newCard);
-        // editaCard(newCard);
-        // nomeiaCard(newCard);
-        // resetaCard(newCard);
-        // removeCard(newCard);
-
-        // todasListas[indiceTrue] = lista;
-        // localStorage.setItem(ListaTipo, JSON.stringify(todasListas));
-
-        var novoElemento = criaConteudo('Elemento ' + ($('.elemento-movivel-container').length + 1) + '');
-
-        customDrag(novoElemento);
-
-        $('section').append(novoElemento);
-    });
-
-    
-
-});
+}
 
 // * Função para configurar regras para movimentação do card
 function customDrag(elemento) {
@@ -58,20 +91,70 @@ function customDrag(elemento) {
 }
 
 // * HTML de um card
-function criaConteudo(nome) {
+function criaConteudo(id) {
     return $(`
         <div class="elemento-movivel-container rounded-circle" style="top: 10%;">
-            <div class="elemento-movivel draggable card">
+            <div id="elemento-movivel-${id}" class="elemento-movivel draggable card">
                 <header class="elemento-movivel-header">
                     <img class="elemento-movivel-imagem" src="./roteador.jpeg"></img>
-                    <div class="elemento-movivel-nome text-center" contenteditable="true">${nome}</div>
+                    <div class="elemento-movivel-nome text-center" contenteditable="true">Elemento ${id}</div>
                 </header>
-                <span class="linkInsert" id="linkInsert1"></span>
-                <span class="linkInsert" id="linkInsert2"></span>
-                <span class="linkInsert" id="linkInsert3"></span>
-                <span class="linkInsert" id="linkInsert4"></span>
+                <span class="linkInsert" id="linkInsert-1"></span>
+                <span class="linkInsert" id="linkInsert-2"></span>
+                <span class="linkInsert" id="linkInsert-3"></span>
+                <span class="linkInsert" id="linkInsert-4"></span>
             </div>
         </div>
-        
     `);
+}
+
+// * Apaga um elemento caso o ambiente esteja em modo exclusão
+function apagaElemento(element) {
+    element.find('.elemento-movivel').click(function () {
+        if ($('#alteraModo i').hasClass('bi-trash2')) {
+            $(this).closest('.elemento-movivel-container').remove();
+            $(this).closest('.elemento-movivel').remove();
+        }
+    });
+}
+
+$(document).ready(function () {
+
+    $('#adicionaElemento').click(function () {
+
+        var id = 1;
+
+        while (true) {
+            if ($('#elemento-movivel-' + id).length == 0) {
+                break;
+            }
+            id++;
+        }
+
+        var novoElemento = criaConteudo(id);
+
+        customDrag(novoElemento);
+        fluxoConecta(novoElemento);
+        apagaElemento(novoElemento);
+        paraTeste(novoElemento);
+
+        $('section').append(novoElemento);
+    });
+    $('#alteraModo').click(function () {
+        console.log("ModoAlterado");
+        $('#alteraModo i').toggleClass('arrows-move bi-trash2');
+        $('#alteraModo span').text(function (i, text) {
+            return text === "Modo Exclusão" ? "Modo Edição" : "Modo Exclusão";
+        });
+    });
+    $('#exportaDados').click(function () {
+        console.log("exportarDados");
+    });
+});
+
+function paraTeste(element) {
+    element.find('.elemento-movivel').click(function (e) {
+        e.preventDefault();
+        console.log("Elemento percebido");
+    });
 }
